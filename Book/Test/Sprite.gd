5378d2c26a7d82f832415b12b28e790dc52019d6
Martin Linklater <mslinklater@gmail.com>
2020-07-12 22:07:42 +0100

    up to chap4

--- Book/Test/Sprite.gd
+++ Book/Test/Sprite.gd
@@ -1,18 +1,19 @@
 extends Sprite
 
-const MOVEMENT_SPEED = 50
+var speed = 0
+var rot_speed = rand_range(-.1,.1)
 
 func _ready():
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	speed += delta * 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var input_direction = 0
-	
-	if Input.is_action_pressed("ui_left"):
-		input_direction = -1
-	elif Input.is_action_pressed("ui_right"):
-		input_direction = 1
+func _process(delta):		
+	position.y += speed * delta
+	rotation += rot_speed
+
+	if position.y > get_viewport().size.y:
+		speed = -abs(speed)
 		
-	position.x += input_direction * MOVEMENT_SPEED * delta
