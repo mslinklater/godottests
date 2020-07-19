79c40cb42e892e3318b0c75e58dd9065f78043c4
Martin Linklater <mslinklater@gmail.com>
2020-07-19 21:56:20 +0100

    Added some tiles

--- Book/Test/Sprite.gd
+++ Book/Test/Sprite.gd
@@ -2,3 +2,20 @@ extends Sprite
 
 const SPEED = 100
 var device_index = 0
+
+func _ready():
+	Input.connect("joy_connection_changed", self, "joy_connect")
+
+func joy_connect(index, connect):
+	if connect:
+		device_index = index
+
+func _process(delta):
+	var direction = Vector2(0,0)
+	
+	if Input.is_joy_button_pressed(device_index, JOY_DPAD_UP):
+		direction.y -= 1.0
+
+	direction *= SPEED * delta
+	translate(direction)
+	
